4ff3554877cca3384f530802b3facb6d8a87ea65
Nemo <dedmazai74@mail.ru>
2012-11-26 10:08:42 +0400

    Added icon, desktop config and installator

---
+++ install.sh
@@ -0,0 +1,5 @@
+mkdir /usr/share/pyshared/Translator
+cp src/Translator/Translator.py /usr/share/pyshared/Translator/Translator.py
+cp src/Translator/images/icon.png /usr/share/pyshared/Translator/images/icon.png
+cp translator.desktop /usr/share/applications/translator.desktop
+
