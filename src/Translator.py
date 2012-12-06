59dc830c3ddc132cab39968c8d6a7b7c2f27820f
Nemo <dedmazai74@mail.ru>
2012-12-06 10:45:51 +0400

    Added audioplayer

--- src/Translator.py
+++ src/Translator.py
@@ -9,22 +9,24 @@ Created on Nov 26, 2012
 
 import gtk
 import httplib
-import re
 import pynotify
+import os
 
-cb = gtk.clipboard_get(gtk.gdk.SELECTION_CLIPBOARD)
-term = cb.wait_for_text().encode('utf-8')
+SRC_LANG = 'en'
+DST_LANG = 'ru'
 
-term = term.replace("\n", "").lstrip().strip()
-term_to_google = term.replace(" ", "%20")
+term = gtk.clipboard_get(gtk.gdk.SELECTION_CLIPBOARD).wait_for_text().encode('utf-8').replace("\n", "").lstrip().strip()
 
 conn = httplib.HTTPConnection("translate.google.com")
-conn.request("GET","/translate_a/t?client=t&text=" + term_to_google + "&hl=en&tl=ru&ie=UTF-8&oe=UTF-8")
-res = conn.getresponse()
-data = res.read()
+conn.request("GET","/translate_a/t?client=t&text=" + term.replace(" ", "%20") + "&hl=" + SRC_LANG + "&tl=" + DST_LANG + "&ie=UTF-8&oe=UTF-8")
+data = conn.getresponse().read()
 
-terms = re.compile('"(.*?)"').findall(data)
+transl = data[data.find('"') + 1 : ]
+transl = transl[ : transl.find('"')]
 
 pynotify.init("Null")
-n = pynotify.Notification (term, terms[0], "Null")
+n = pynotify.Notification (term, transl, "Null")
 n.show()
+
+os.system("cvlc --play-and-exit 'http://translate.google.com/translate_tts?ie=UTF-8&q=" + term.replace(" ", "%20") + "&tl=" + SRC_LANG + "&total=1&idx=0&textlen=5'")
+os.system("cvlc --play-and-exit 'http://translate.google.com/translate_tts?ie=UTF-8&q=" + transl + "&tl=" + DST_LANG + "&total=1&idx=0&textlen=5'")
